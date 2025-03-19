a4c68a206ff2d55d9787eff46cfc8ddbf4a6883e
Sendiko <sarangtawon897@gmail.com>
2025-03-19 23:53:34 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/src/views"));
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files
+
+// Middleware to use layout
+function renderWithLayout(res, view, options = {}) {
+  res.render("layouts/main", {
+    ...options,
+    body: `pages/${view}`, // Inject the page inside the layout
+  });
+}
+
+// Routes
+app.get("/", (req, res) => {
+  renderWithLayout(res, "home", { title: "Home" });
+});
+
+app.get("/about", (req, res) => {
+  renderWithLayout(res, "about", { title: "About Us" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
